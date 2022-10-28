2a3be9bda9979a2b73f40153b56a9d97f88d09ff
timiwritecodes <113919868+timiwritecodes@users.noreply.github.com>
2022-10-28 07:33:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "bf72f545145a1f74b38e5667"
+
+
+@app.route('/', methods=['GET'])
+@cross_origin()
+def get_info():
+    info = {
+        "slackUsername": "zion",
+        "backend": "True",
+        "age": 20,
+        "bio": 'I am zion, a civil engineering undergraduate at FUTA, I\'m in the backend track of hng9.',
+    }
+
+    return jsonify(info)
+
+
+if __name__ == '__main__':
+    app.run()
