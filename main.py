84bc25ed973766f5048339d193af5daaa1dc9260
timiwritecodes <113919868+timiwritecodes@users.noreply.github.com>
2022-10-29 10:26:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "bf72f545145a1f74b38e5667"
 app.config['JSON_SORT_KEYS'] = False
 
-
 @app.route('/', methods=['GET'])
 @cross_origin()
 def get_info():
     info = {
-        "slackUsername": "zion",
+        "slackUsername": "zion01",
         "backend": True,
         "age": 20,
         "bio": 'I am zion, a civil engineering undergraduate at FUTA, I\'m in the backend track of hng9.',
     }
 
-    return info
+    return jsonify(info)
 
 
 if __name__ == '__main__':
