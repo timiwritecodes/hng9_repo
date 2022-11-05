27aa79d98c7dab4f37db6a655a5efab7a98f12d4
timiwritecodes <113919868+timiwritecodes@users.noreply.github.com>
2022-11-05 23:57:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import cross_origin
+from enum import Enum
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "bf72f545145a1f74b38e5667"
@@ -16,6 +18,54 @@ def get_info():
     }
 
     return jsonify(info)
+
+
+@app.route('/operations', methods=['POST'])
+@cross_origin()
+def evaluate_integer():
+    request_data = request.get_json()
+
+    slackUsername = "zion01"
+    x = None
+    y = None
+
+    result = None
+    op = request.json['operation_type']
+    print(type(op))
+    print(op)
+    if request_data:
+        if 'x' in request_data:
+            x = request_data['x']
+        if 'y' in request_data:
+            y = request_data['y']
+
+    class Calc(Enum):
+        ADD = "addition"
+        SUB = "subtraction"
+        MULT = "multiplication"
+
+    operations = [item.value for item in Calc]
+
+    operations_dict = {'addition': x+y, 'multiplication': x*y, 'subtraction': x-y}
+
+    if request_data:
+        if 'operation_type' in request_data:
+            if op in operations:
+                result = operations_dict[op]
+            else:
+                return {
+                    "message": "your operator_type should be 'add', 'subtract', 'multiply'"
+                        }
+        else:
+            return {
+                "message": "operation_type field is empty"
+            }
+
+    return {
+            "slackUsername": slackUsername,
+            "result": result,
+            "operation_type": op
+                }
 
 
 if __name__ == '__main__':
